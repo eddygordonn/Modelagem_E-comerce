6442d89da6bf1a0d9391af2ef438c59040e76f8e
Eddy_Gordon <eddygordonn@gmail.com>
2025-04-09 19:25:13 -0300

    Update Selects.sql

--- Querys/Selects.sql
+++ Querys/Selects.sql
@@ -23,3 +23,58 @@ JOIN Produto_Estoque pe ON pr.id_produto = pe.id_produto
 JOIN Estoque e ON pe.id_estoque = e.id_estoque
 WHERE pe.quantidade > 0
 ORDER BY pr.nome;
+
+--Listar Todos os Clientes (PF e PJ)
+SELECT 
+    c.id_cliente,
+    c.tipo,
+    IF(c.tipo = 'PF', pf.nome, pj.razao_social) AS nome_razao_social,
+    IF(c.tipo = 'PF', pf.CPF, pj.CNPJ) AS documento
+FROM 
+    Cliente c
+LEFT JOIN Cliente_PF pf ON c.id_cliente = pf.id_cliente AND c.tipo = 'PF'
+LEFT JOIN Cliente_PJ pj ON c.id_cliente = pj.id_cliente AND c.tipo = 'PJ';
+
+--Listar Todos os Produtos com Preço > R$ 1000
+SELECT nome, preco
+FROM Produto
+WHERE preco > 1000
+ORDER BY preco DESC;
+
+--Pedidos com Status "Enviado" ou "Entregue"
+SELECT 
+    p.id_pedido,
+    c.tipo AS tipo_cliente,
+    IF(c.tipo = 'PF', pf.nome, pj.razao_social) AS cliente,
+    p.status
+FROM 
+    Pedido p
+JOIN Cliente c ON p.id_cliente = c.id_cliente
+LEFT JOIN Cliente_PF pf ON c.id_cliente = pf.id_cliente AND c.tipo = 'PF'
+LEFT JOIN Cliente_PJ pj ON c.id_cliente = pj.id_cliente AND c.tipo = 'PJ'
+WHERE 
+    p.status IN ('Enviado', 'Entregue');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
